c99b010fca224a305032d7e9f7ffee2a1225f1df
daominhkhanh20 <khanhc1k36@gmail.com>
2022-06-30 10:39:43 +0700

    update unittest

--- test/test_bert_infer.py
+++ test/test_bert_infer.py
@@ -1,13 +1,28 @@
 from src.inference import SentimentInference
-
-inference = SentimentInference(
-    model_path='assets/models/BertModel'
-)
-
-print(inference.predict("Cô dạy khá hay"))
+import unittest
 
 """
+inference.predict("Cô dạy khá hay")
+
 Result:
  {'pred_label': 'positive', 
  'probability': [0.9999295473098755, 5.33430738869356e-06, 6.507167563540861e-05]}
 """
+
+
+class TestingModel(unittest.TestCase):
+    def setUp(self):
+        self.inference = SentimentInference(
+            model_path='assets/models/BertModel'
+        )
+
+    def test_model_bert(self):
+        test_case = {
+            "Cô dạy khá hay": "positive",
+            "bình thường": "neural",
+            "abcxyz abcxyz": "neural",
+            "cô hơi già": "negative"
+        }
+        for sent, label in test_case.items():
+            log = self.inference.predict(sent)
+            self.assertEqual(log['pred_label'], label)
